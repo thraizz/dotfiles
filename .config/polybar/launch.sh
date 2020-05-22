8176c32910a2affd413eb7fa33ea26a75f680bf6
Aron Schüler <schueleraron@googlemail.com>
2020-05-23 00:40:17 +0200

    Switch to polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar at bottom
+polybar top
+polybar bottom
+
