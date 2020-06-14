27c3dd1824e5491ebbacf5eeacf12bab3d471e52
Aron Schüler <schueleraron@googlemail.com>
2020-06-14 18:31:26 +0200

    Fix polybar launch configuration

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar at bottom
-polybar top
-polybar bottom
-
+# Launch bars
+echo "--- RESTARTED ---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar dp1 >>/tmp/polybar.log 2>&1 &
+polybar dp5 >>/tmp/polybar.log 2>&1 &
+polybar macbook >>/tmp/polybar.log 2>&1 &
