bdc5d30b231c6843edb9786f327e5d05f1599959
Aron Schüler <schueleraron@googlemail.com>
2020-06-17 10:56:58 +0200

    Fix i3 workspaces script

--- scripts/i3-workspace.sh
+++ scripts/i3-workspace.sh
@@ -4,14 +4,18 @@ action="$1"
 workspace="$2"
 
 focused_workspace=$(i3-msg -t get_workspaces | jq --raw-output '.[]|select(.focused).name')
-
+echo $focused_workspace
 active_postfix=${focused_workspace: -1}
 
 target_workspace=${workspace}${active_postfix}
+echo $target_workspace
 
-if [[ "$action" == "move" ]]; 
-then
+if [[ "$focused_workspace" == "$target_workspace" ]]; then
+	target_workspace=$(i3-msg -t get_workspaces | jq --raw-output '.[]|select(.focused == false and .visible).name')
+fi
+
+if [[ "$action" == "move" ]]; then
     i3-msg "move container to workspace $target_workspace"
-    else
-        i3-msg "workspace $target_workspace"
+else
+    i3-msg "workspace $target_workspace"
 fi
