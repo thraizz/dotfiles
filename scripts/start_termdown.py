e321ad83769eca6d38dbfc89e92f52ac864a8284
Aron Schüler <schueleraron@googlemail.com>
2020-06-21 13:25:33 +0200

    Add timer script

---
+++ scripts/start_termdown.py
@@ -0,0 +1,15 @@
+#!/bin/python
+
+import subprocess
+
+def callRofi(title="Pick one"):
+    c =  'rofi -width 200 -lines 0 -location 0 -dmenu -p "{}" -padding 20 -line-margin 10'.format(title)
+    proc = subprocess.Popen(c, shell=True,stdout=subprocess.PIPE)
+    choice = proc.stdout.readline().decode("UTF-8")
+    return choice[:-1]
+
+time = callRofi("How long?")
+
+c = 'xfce4-terminal --title="Timer" --zoom=2 -x termdown {} --no-figlet'.format(time)
+c = c+"; i3-msg '[title=Timer] resize set 200 80'"
+subprocess.Popen(c, shell=True)
