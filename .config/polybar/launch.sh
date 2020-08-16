88551563f74f7a6b28aaaf88c60eae51851a5881
Aron Schüler <schueleraron@googlemail.com>
2020-08-16 14:43:47 +0200

    [Bar] Add ewmh/openbox settings

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,6 +8,11 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bars
 echo "--- RESTARTED ---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar dp1-i3 >>/tmp/polybar.log 2>&1 & disown;
-polybar dp4-i3 >>/tmp/polybar.log 2>&1 & disown;
+if [[ $DESKTOP_SESSION == "openbox" ]]; then
+    polybar dp1-ewmh >>/tmp/polybar.log 2>&1 & disown;
+    polybar dp4-ewmh >>/tmp/polybar.log 2>&1 & disown;
+else
+    polybar dp1-i3 >>/tmp/polybar.log 2>&1 & disown;
+    polybar dp4-i3 >>/tmp/polybar.log 2>&1 & disown;
+fi
 MONITOR=$(xrandr --query | grep primary | grep -Eo '^e[^ ]+') polybar laptop >>/tmp/polybar.log 2>&1 & disown;
