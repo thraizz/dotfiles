914d49005093d535f0974bdbdbbf207de6653df3
Aron Schüler <schueleraron@googlemail.com>
2020-08-16 14:44:30 +0200

    [Ranger] Improve ranger find command

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -158,8 +158,7 @@ class fzf_select(Command):
             -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m --reverse --header='Jump to file'"
         else:
             # match files and directories
-            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m --reverse --header='Jump to filemap <C-f> fzf_select'"
+            command="fd --type f --hidden --follow --exclude .git | fzf +m --reverse --header='Jump to filemap <C-f> fzf_select'"
         fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
