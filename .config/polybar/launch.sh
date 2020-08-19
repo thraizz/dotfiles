756acf17bb94c4851cfca23653cc801ccc06ada7
Aron Schueler <schueleraron@lakur.tech>
2020-08-19 16:24:04 +0200

    Update after arch installation

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,10 +9,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bars
 echo "--- RESTARTED ---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 if [[ $DESKTOP_SESSION == "openbox" ]]; then
-    polybar dp1-ewmh >>/tmp/polybar.log 2>&1 & disown;
-    polybar dp4-ewmh >>/tmp/polybar.log 2>&1 & disown;
+    polybar main-ewmh >>/tmp/polybar.log 2>&1 & disown;
+    polybar secondary-ewmh >>/tmp/polybar.log 2>&1 & disown;
 else
-    polybar dp1-i3 >>/tmp/polybar.log 2>&1 & disown;
-    polybar dp4-i3 >>/tmp/polybar.log 2>&1 & disown;
+    polybar main-i3 >>/tmp/polybar.log 2>&1 & disown;
+    polybar secondary-i3 >>/tmp/polybar.log 2>&1 & disown;
 fi
 MONITOR=$(xrandr --query | grep primary | grep -Eo '^e[^ ]+') polybar laptop >>/tmp/polybar.log 2>&1 & disown;
