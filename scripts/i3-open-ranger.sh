a6f770171adc2e96d82891ea3caa3dd3abac4929
Aron Schueler <schueleraron@lakur.tech>
2020-08-20 13:05:44 +0200

    Replace ALL absolute ranger paths and add icon

--- scripts/i3-open-ranger.sh
+++ scripts/i3-open-ranger.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 i3-msg -t get_tree | jq --raw-output | grep ranger-floating
 if [ $? -ne 0 ]; then
-    xfce4-terminal -T ranger-floating -x /home/arons/scripts/start_ranger.sh
+    xfce4-terminal -T ranger-floating -x $HOME/scripts/start_ranger.sh
 else
     i3-msg [title="ranger-floating"] focus
 fi
