a8c7012222cf5f6230cd90fdbf5dab57c3f59c76
Aron Schueler <schueleraron@lakur.tech>
2020-08-26 10:39:20 +0200

    Update wallpaper script to use public endpoint

--- scripts/newwallpaper.sh
+++ scripts/newwallpaper.sh
@@ -1,6 +1,4 @@
 #!/bin/zsh
 export DISPLAY=:0.0
-source /etc/environment
-url=$(curl -s "https://api.unsplash.com/photos/random?client_id=$UNSPLASH_TOKEN&collections=219941,9827255,1201327&orientation=landscape" | jq --raw-output ".urls.raw")
-curl -s -o $HOME/Wallpapers/current.png $url
+curl -L -o $HOME/Wallpapers/current.png "https://source.unsplash.com/random?nature/2560x1440" 
 wal -i $HOME/Wallpapers/current.png --saturate 1 -e
