ac3adc173c4f5e589ef91553bbeea0efe1adf57a
Aron Schueler <schueleraron@lakur.tech>
2020-08-28 09:16:47 +0200

    Various updates

--- scripts/newwallpaper.sh
+++ scripts/newwallpaper.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
 export DISPLAY=:0.0
-curl -L -o $HOME/Wallpapers/current.png "https://source.unsplash.com/random?nature/2560x1440" 
+source /etc/environment
+url=$(curl -s "https://api.unsplash.com/photos/random?client_id=$UNSPLASH_TOKEN&collections=219941,9827255,1201327&orientation=landscape" | jq --raw-output ".urls.full")
+curl -s -o $HOME/Wallpapers/current.png $url
 wal -i $HOME/Wallpapers/current.png --saturate 1 -e
