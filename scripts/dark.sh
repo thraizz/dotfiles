47c01afb9dc20d3d0ce92211da0988fce3af09ef
A. Schueler <schueleraron@lakur.tech>
2020-10-20 19:38:08 +0200

    Add scripts to toggle dark/light theme

---
+++ scripts/dark.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+DISPLAY=":0.0"
+cp $HOME/.config/kitty/dark.conf $HOME/.config/kitty/current.conf
+cp $HOME/.vim/colors/dark.vim $HOME/.vim/colors/current.vim
+feh --bg-fill --randomize /home/dubai/Wallpapers/Dark/*
---
+++ scripts/light.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+DISPLAY=":0.0"
+cp $HOME/.config/kitty/light.conf $HOME/.config/kitty/current.conf
+cp $HOME/.vim/colors/light.vim $HOME/.vim/colors/current.vim
+feh --bg-fill --randomize ~/Wallpapers/Light/*
