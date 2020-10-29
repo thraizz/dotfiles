9e60d119d9552e11b7086a5668a1d23efa237c8e
Aron Schüler <schueleraron@lakur.tech>
2020-10-29 11:26:31 +0100

    Switch to gruvbox

--- scripts/dark.sh
+++ scripts/dark.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 DISPLAY=":0.0"
+export DARK='True'
 cp $HOME/.config/kitty/dark.conf $HOME/.config/kitty/current.conf
-cp $HOME/.vim/colors/dark.vim $HOME/.vim/colors/current.vim
+cp $HOME/.config/nvim/colors/gruvbox.vim $HOME/.vim/colors/current.vim
 feh --bg-fill --randomize /home/dubai/Wallpapers/Dark/*
+wal --theme gruvbox
--- scripts/light.sh
+++ scripts/light.sh
@@ -3,3 +3,4 @@ DISPLAY=":0.0"
 cp $HOME/.config/kitty/light.conf $HOME/.config/kitty/current.conf
 cp $HOME/.vim/colors/light.vim $HOME/.vim/colors/current.vim
 feh --bg-fill --randomize ~/Wallpapers/Light/*
+wal --theme one-half-light -l
