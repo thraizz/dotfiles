4a8ce000fc7911b810b93c2966daac5578344b77
Aron Schüler <schueleraron@lakur.tech>
2020-10-30 11:57:49 +0100

    Switch to light gruvbox theme in light script

--- scripts/light.sh
+++ scripts/light.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 DISPLAY=":0.0"
 cp $HOME/.config/kitty/light.conf $HOME/.config/kitty/current.conf
-cp $HOME/.vim/colors/light.vim $HOME/.vim/colors/current.vim
 feh --bg-fill --randomize ~/Wallpapers/Light/*
-wal --theme one-half-light -l
+wal --theme base16-gruvbox-hard -l
