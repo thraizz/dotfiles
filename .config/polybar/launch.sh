2e0460418037fd40c03a1ff011af06754933b845
Aron Schüler <schueleraron@lakur.tech>
2020-11-22 10:16:20 +0100

    Optimize polybar launch

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,10 +5,9 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Get monitors
 primary=$(xrandr --query | grep -P ' connected primary' | cut -d' ' -f1)
-secondary=$(xrandr --query | grep -P ' connected (?!primary)' | cut -d' ' -f1)
+secondary=$(xrandr --query | grep -P '^(?!e).* connected (?!primary)' | cut -d' ' -f1)
 
 # Launch bars
-echo "--- RESTARTED ---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 if [[ $DESKTOP_SESSION == "openbox" ]]; then
     MONITOR=$primary polybar main-ewmh >>/tmp/polybar.log 2>&1 & disown;
     MONITOR=$secondary polybar secondary-ewmh >>/tmp/polybar.log 2>&1 & disown;
@@ -16,6 +15,9 @@ if [[ $DESKTOP_SESSION == "openbox" ]]; then
 else
     MONITOR=$primary polybar main-i3 >>/tmp/polybar.log 2>&1 & disown;
     MONITOR=$secondary polybar secondary-i3 >>/tmp/polybar.log 2>&1 & disown;
-    exit 0
 fi
-MONITOR=$(xrandr --query | grep primary | grep -Eo '^e[^ ]+') polybar laptop >>/tmp/polybar.log 2>&1 & disown;
+laptop=$(xrandr --query | grep -P '^e.* connected' | cut -d' ' -f1)
+MONITOR=$laptop polybar laptop >>/tmp/polybar.log 2>&1 & disown;
+
+
+echo "--- RESTARTED $primary $secondary $laptop ---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
