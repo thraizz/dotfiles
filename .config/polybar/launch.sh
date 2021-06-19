32505c3adca733a52117cb7d82c23470ba5efb9b
Aron Schuler <aron_marian.schueler@smail.th-koeln.de>
2021-06-19 11:07:29 +0200

    Remove check for openbox in polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,16 +8,10 @@ primary=$(xrandr --query | grep -P ' connected primary' | cut -d' ' -f1)
 secondary=$(xrandr --query | grep -P '^(?!e).* connected (?!primary)' | cut -d' ' -f1)
 
 # Launch bars
-if [[ $DESKTOP_SESSION == "openbox" ]]; then
-    MONITOR=$primary polybar main-ewmh >>/tmp/polybar.log 2>&1 & disown;
-    MONITOR=$secondary polybar secondary-ewmh >>/tmp/polybar.log 2>&1 & disown;
-    exit 0
-else
-    MONITOR=$primary polybar main-i3 >>/tmp/polybar.log 2>&1 & disown;
-    MONITOR=$secondary polybar secondary-i3 >>/tmp/polybar.log 2>&1 & disown;
+if [[ -v primary ]]; then
+  MONITOR=$primary polybar main-i3 >>/tmp/polybar.log 2>&1 & disown;
 fi
-laptop=$(xrandr --query | grep -P '^e.* connected' | cut -d' ' -f1)
-MONITOR=$laptop polybar laptop >>/tmp/polybar.log 2>&1 & disown;
-
-
-echo "--- RESTARTED $primary $secondary $laptop ---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+if [[ -v secondary ]]; then
+  MONITOR=$secondary polybar secondary-i3 >>/tmp/polybar.log 2>&1 & disown;
+fi
+exit 0
