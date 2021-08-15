688f12196216582ebf9f3fe7f6729686a301b496
Aron Schuler <schueleraron@lakur.tech>
2021-08-15 14:22:46 +0200

    Merge remote master into local

--- scripts/light.sh
+++ scripts/light.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 DISPLAY=":0.0"
-cp $HOME/.config/kitty/light.conf $HOME/.config/kitty/current.conf
 feh --bg-fill --randomize ~/Wallpapers/Light/*
 wal --theme base16-gruvbox-hard -l
-pywalfox update
+cp $HOME/.cache/wal/colors-kitty.conf ~/.config/kitty/current.conf
