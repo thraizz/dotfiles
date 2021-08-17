971b660e89e98e7ae04976752fca70ce675ac07d
Aron Schuler <schueleraron@lakur.tech>
2021-08-17 16:12:41 +0200

    Switch to gruvbox rofi theme

--- scripts/start_rofi.sh
+++ scripts/start_rofi.sh
@@ -1,2 +1,2 @@
-rofi -show combi -combi-modi window,drun,ssh,run -modi combi -window-match-field title -window-format "{n} {t}" -show-icons -theme $HOME/.config/rofi/themes/slate.rasi
+rofi -show combi -combi-modi window,drun,ssh,run -modi combi -window-match-field title -window-format "{n} {t}" -show-icons -theme $HOME/.config/rofi/themes/gruvbox-hard.rasi
 #-sort -levenshstein-sort
