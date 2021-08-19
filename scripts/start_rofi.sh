07b899dd1a347fd7e7e98bae28a6e1bd8a2339ab
Aron Schuler <schueleraron@lakur.tech>
2021-08-19 21:48:35 +0200

    Rofi: Improve sorting and use primary monitor

--- scripts/start_rofi.sh
+++ scripts/start_rofi.sh
@@ -1,2 +1,2 @@
-rofi -show combi -combi-modi window,drun,ssh,run -modi combi -window-match-field title -window-format "{n} {t}" -show-icons -theme $HOME/.config/rofi/themes/gruvbox-hard.rasi
+rofi -show combi -combi-modi window,drun,ssh,run -modi combi -window-match-field title -window-format "{n} {t}" -show-icons -theme $HOME/.config/rofi/themes/gruvbox-hard.rasi -m 0 -sorting-method fzf
 #-sort -levenshstein-sort
