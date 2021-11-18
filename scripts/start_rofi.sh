5adef2658328e63586a0da47afbfaa6ff8349752
Aron Schüler <schueleraron@lakur.tech>
2021-11-18 01:00:41 +0100

    Show rofi on active monitor

--- scripts/start_rofi.sh
+++ scripts/start_rofi.sh
@@ -1,2 +1,2 @@
-rofi -show combi -combi-modi window,drun,ssh,run -modi combi -window-match-field title -window-format "{n} {t}" -show-icons -theme $HOME/.config/rofi/themes/gruvbox-hard.rasi -m 0 -sorting-method fzf -drun-match-fields name,generic
+rofi -show combi -combi-modi window,drun,ssh,run -modi combi -window-match-field title -window-format "{n} {t}" -show-icons -theme $HOME/.config/rofi/themes/gruvbox-hard.rasi -sorting-method fzf -drun-match-fields name,generic
 #-sort -levenshstein-sort
