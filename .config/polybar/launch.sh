01a20279e87035ba7d6cc8beec504f8c0f284a87
Aron Schüler <schueleraron@lakur.tech>
2021-12-01 11:19:35 +0100

    Update polybar configuration

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,7 +5,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-laptop=$(xrandr --query | grep -P '^e.* connected' | cut -d' ' -f1)
+laptop=$(xrandr --query | grep -P '^e.* connected (?!primary)' | cut -d' ' -f1)
 if [[ $laptop ]]; then
   MONITOR=$laptop polybar laptop >>/tmp/polybar.log 2>&1 & disown;
 fi
@@ -19,7 +19,9 @@ if [[ $primary ]]; then
   MONITOR=$primary polybar main-i3 >>/tmp/polybar.log 2>&1 & disown;
 fi
 if [[ $secondary ]]; then
-  MONITOR=$secondary polybar secondary-i3 >>/tmp/polybar.log 2>&1 & disown;
+  for monitor in $(xrandr --query | grep -P '^(?!e).* connected (?!primary)' | cut -d' ' -f1); do
+    MONITOR=$monitor polybar secondary-i3 >>/tmp/polybar.log 2>&1 & disown;
+  done
 fi
 echo "--- RESTARTED $primary $secondary $laptop ---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 exit 0
